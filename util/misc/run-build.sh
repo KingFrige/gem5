f35abe25b1d026ae37ceeb195d42a17801ea565c
korben.dong <ghostyuan@outlook.com>
2025-09-19 19:14:57 +0800

    misc: update scripts

--- util/misc/run-build.sh
+++ util/misc/run-build.sh
@@ -6,9 +6,9 @@ verision=${2:-debug}
 if [[ ${ARCH} == "TOTAL" ]]; then
   for arch in ALL X86 RISCV ARM; do
     for ver in opt debug fast; do
-      time /bin/scons-3 -sQ build/${arch}/gem5.${ver} --gold-link -j4
+      time scons -sQ build/${arch}/gem5.${ver} --gold-link -j4
     done
   done
 else
-  time /bin/scons-3 -sQ build/${ARCH}/gem5.${verision} --gold-link -j4
+  time scons -sQ build/${ARCH}/gem5.${verision} --gold-link -j4
 fi
